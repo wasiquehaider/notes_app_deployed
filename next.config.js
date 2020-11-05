805ec876116dcc0633ee3a76f0931c38cb660952
waiquedd <whaider@digital-dividend.com>
2020-11-05 11:41:37 +0500

    Next JS App to Netlify

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    MONGO_URI:
+      "mongodb+srv://wasique:admin123@cluster0.btqiv.mongodb.net/notesDB?retryWrites=true&w=majority",
+  },
+  target: "serverless",
+};
